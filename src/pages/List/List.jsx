5e3b3fcb08b3955940e805db3f9058fe8d4db86c
ALAGU THANUSH <104085246+thanush7@users.noreply.github.com>
2024-06-18 15:55:37 +0530

    Update List.jsx

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 const List = () => {
   
-  const url='http://localhost:4000';
+  const url='https://food-backend-s5to.onrender.com';
   const [list,setList]=useState([]);
   const fetchList=async ()=>{
     const response=await axios.get(`${url}/api/food/list`);
