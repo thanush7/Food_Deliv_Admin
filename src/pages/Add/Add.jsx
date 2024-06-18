1bcafaa0be920b8db1981af0ffb5f34a91283741
ALAGU THANUSH <104085246+thanush7@users.noreply.github.com>
2024-06-18 15:55:17 +0530

    Update Add.jsx

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -4,7 +4,7 @@ import { assets } from '../../assets/assets'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 const Add = () => {
-    const url='http://localhost:4000';
+    const url='https://food-backend-s5to.onrender.com';
     const [image,setImage]=useState(false);
     const [data,setData]=useState(
         {
